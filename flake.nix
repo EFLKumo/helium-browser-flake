cb1e1461bcbf2e6f38037066cb282bebedb053e7
imxyy_soope_ <103114856+imxyy1soope1@users.noreply.github.com>
2025-11-02 14:44:15 +0800

    fix: wrapQtApp (#1)
    * fmt: use nixfmt
    
    * chore: add .gitignore
    
    * chore: remove cachix config
    
    * fix: wrapQtApp

--- flake.nix
+++ flake.nix
@@ -1,19 +1,18 @@
 {
   description = "Flake for Helium Browser";
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux"];
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       imports = [
         ./package.nix
         ./treefmt.nix
       ];
     };
-
-  nixConfig = {
-    extra-substituters = ["https://cache.garnix.io"];
-    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
-  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
--- package.nix
+++ package.nix
@@ -1,152 +1,165 @@
-{...}: {
-  perSystem = {
-    pkgs,
-    system,
-    ...
-  }: let
-    info = (builtins.fromJSON (builtins.readFile ./sources.json)).${system};
+{
+  perSystem =
+    {
+      pkgs,
+      system,
+      ...
+    }:
+    let
+      info = (builtins.fromJSON (builtins.readFile ./sources.json)).${system};
 
-    pname = "helium";
-    version = info.version;
+      pname = "helium";
+      version = info.version;
 
-    src-appimage = pkgs.fetchurl {
-      url = info.appimage_url;
-      hash = info.appimage_sha256;
-    };
+      src-appimage = pkgs.fetchurl {
+        url = info.appimage_url;
+        hash = info.appimage_sha256;
+      };
 
-    helium-appimage = pkgs.appimageTools.wrapType2 {
-      inherit version;
-      pname = "${pname}-appimage";
-      src = src-appimage;
+      helium-appimage = pkgs.appimageTools.wrapType2 {
+        inherit version;
+        pname = "${pname}-appimage";
+        src = src-appimage;
 
-      nativeBuildInputs = [pkgs.copyDesktopItems];
-      desktopItems = [
-        (pkgs.makeDesktopItem {
+        nativeBuildInputs = [ pkgs.copyDesktopItems ];
+        desktopItems = [
+          (pkgs.makeDesktopItem {
           })
-      ];
+        ];
+      };
+
+      src = pkgs.fetchurl {
+        url = info.tar_url;
+        hash = info.tar_sha256;
+      };
+
+      helium = pkgs.stdenv.mkDerivation {
+        inherit pname version src;
+
+        desktopItems = [
+          (pkgs.makeDesktopItem {
+            name = "helium-browser";
+            desktopName = "Helium";
+            exec = "helium %U";
+            startupWMClass = "helium-browser";
+            startupNotify = true;
+            mimeTypes = [
+              "application/pdf"
+              "application/rdf+xml"
+              "application/rss+xml"
+              "application/xhtml+xml"
+              "application/xhtml_xml"
+              "application/xml"
+              "image/gif"
+              "image/jpeg"
+              "image/png"
+              "image/webp"
+              "text/html"
+              "text/xml"
+              "x-scheme-handler/http"
+              "x-scheme-handler/https"
+              "x-scheme-handler/webcal"
+              "x-scheme-handler/mailto"
+              "x-scheme-handler/about"
+              "x-scheme-handler/unknown"
+            ];
+          })
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          autoPatchelfHook
+          patchelfUnstable
+          copyDesktopItems
+          qt6.wrapQtAppsHook
+        ];
+
+        # borrowed from nixpkgs
+        buildInputs = with pkgs; [
+          qt6.qtbase
+
+          bzip2
+          flac
+          speex
+          libevent
+          expat
+          libjpeg
+          snappy
+          libcap
+          minizip
+          libwebp
+          libusb1
+          re2
+          ffmpeg
+          libxslt
+          libxml2
+          nasm
+          nspr
+          nss
+          util-linux
+          alsa-lib
+          libkrb5
+          glib
+          gtk3
+          dbus-glib
+          xorg.libXScrnSaver
+          xorg.libXcursor
+          xorg.libXtst
+          xorg.libxshmfence
+          libGLU
+          libGL
+          dri-pkgconfig-stub
+          libgbm
+          pciutils
+          protobuf
+          speechd-minimal
+          xorg.libXdamage
+          at-spi2-core
+          pipewire
+          libva
+          libdrm
+          wayland
+          libxkbcommon
+          curl
+          libepoxy
+          libffi
+          libevdev
+
+          systemd
+
+          libgcrypt
+          cups
+        ];
+
+        runtimeDependencies = with pkgs; [ libGL ];
+
+        patchelfFlags = [ "--no-clobber-old-sections" ];
+        autoPatchelfIgnoreMissingDeps = [
+          "libQt5Core.so.5"
+          "libQt5Gui.so.5"
+          "libQt5Widgets.so.5"
+        ];
+
+        installPhase = ''
+          runHook preInstall
+
+          libExecPath="$prefix/libexec/${pname}-bin-$version"
+          mkdir -p "$libExecPath"
+          cp -rv ./ "$libExecPath/"
+
+          mkdir -p $out/bin
+          wrapQtApp "$libExecPath/chrome-wrapper" \
+            --prefix LD_LIBRARY_PATH : "$rpath"
+          ln -s "$libExecPath/chrome-wrapper" "$out/bin/${pname}"
+
+          runHook postInstall
+        '';
+      };
+    in
+    {
+      packages = {
+        inherit helium helium-appimage;
+
+        default = helium;
+      };
     };
-
-    src = pkgs.fetchurl {
-      url = info.tar_url;
-      hash = info.tar_sha256;
-    };
-
-    helium = pkgs.stdenv.mkDerivation {
-      inherit pname version src;
-
-      nativeBuildInputs = with pkgs; [
-        autoPatchelfHook
-        patchelfUnstable
-        copyDesktopItems
-        kdePackages.wrapQtAppsHook
-        makeWrapper
-      ];
-
-      buildInputs = with pkgs; [
-        libgbm
-        glibc
-        glib
-        dbus
-        expat
-        cups
-        nspr
-        nss
-        libx11
-        libxcb
-        libxext
-        libxfixes
-        libxrandr
-        cairo
-        pango
-        at-spi2-atk
-        atk
-        gtk3
-        alsa-lib
-        at-spi2-core
-        qt6.qtbase
-        vulkan-loader
-        libva
-        libvdpau
-        libglvnd
-        mesa
-        glib
-        fontconfig
-        freetype
-        pango
-        cairo
-        libx11
-        atk
-        nss
-        nspr
-        libxcursor
-        libxext
-        libxfixes
-        libxrender
-        libxcb
-        alsa-lib
-        expat
-        cups
-        dbus
-        gdk-pixbuf
-        gcc-unwrapped.lib
-        systemd
-        libexif
-        pciutils
-        liberation_ttf
-        curl
-        util-linux
-        wget
-        flac
-        harfbuzz
-        icu
-        libpng
-        snappy
-        speechd
-        bzip2
-        libcap
-        at-spi2-atk
-        at-spi2-core
-        libkrb5
-        libdrm
-        libglvnd
-        libgbm
-        coreutils
-        libxkbcommon
-        pipewire
-        wayland
-      ];
-
-      runtimeDependencies = with pkgs; [libGL];
-
-      appendRunpaths = [
-        "${pkgs.libGL}/lib"
-        "${pkgs.mesa}/lib"
-        "${pkgs.vulkan-loader}/lib"
-        "${pkgs.libva}/lib"
-        "${pkgs.libvdpau}/lib"
-      ];
-
-      patchelfFlags = ["--no-clobber-old-sections"];
-      autoPatchelfIgnoreMissingDeps = ["libQt5Core.so.5" "libQt5Gui.so.5" "libQt5Widgets.so.5"];
-
-      installPhase = ''
-        runHook preInstall
-
-        libExecPath="$prefix/lib/${pname}-bin-$version"
-        mkdir -p "$libExecPath"
-        cp -rv ./ "$libExecPath/"
-
-        makeWrapper "$libExecPath/chrome-wrapper" "$out/bin/${pname}" \
-          --prefix LD_LIBRARY_PATH : "$rpath"
-
-        runHook postInstall
-      '';
-    };
-  in {
-    packages = {
-      inherit helium helium-appimage;
-
-      default = helium;
-    };
-  };
 }
